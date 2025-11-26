1abee3b8a42ba0fc1c8e0498d741b227a27da74b
oudarja <oudarja360@gmail.com>
2025-11-26 10:36:58 +0600

    password hidden

--- database.py
+++ database.py
@@ -2,11 +2,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import pymysql
-
+from dotenv import load_dotenv
+import os
+load_dotenv() 
+DB_PASS = os.getenv("password")
 # Use pymysql as MySQL driver
 pymysql.install_as_MySQLdb()
 
-DATABASE_URL = "mysql+pymysql://root:destinationred002@localhost:3306/newsbd2"
+DATABASE_URL = f"mysql+pymysql://root:{DB_PASS}@localhost:3306/newsbd2"
 
 # Create engine
 engine = create_engine(DATABASE_URL, echo=False)
